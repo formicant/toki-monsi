910a40d4e3eabc3a5e4fd77d8593e293ad7d6823
formicant <formicant@gmail.com>
2022-01-12 11:54:36 +0700

    Append to list instead of yielding

--- palindrome.py
+++ palindrome.py
@@ -10,17 +10,18 @@ class PalindromeGenerator:
     def generate(self, max_word_count: int) -> list[str]:
         """ Returns a list of all possible palindromes with <= `max_word_count` words.
         """
-        palindromes = (' '.join(words)
-            for node, word in self.graph.start_nodes
-            for words in self._get_palindromes_recursively(node, [word], max_word_count))
-        return list(palindromes)
+        palindromes: list[str] = []
+        for node, word in self.graph.start_nodes:
+            self._get_palindromes_recursively(node, [word], max_word_count, palindromes)
+        
+        return palindromes
     
-    def _get_palindromes_recursively(self, node: Node, words: list[str], max_word_count: int) -> Iterable[list[str]]:
+    def _get_palindromes_recursively(self, node: Node, words: list[str], max_word_count: int, output: list[str]):
         distance = self.graph.distances[node]
         if distance < max_word_count:
             if distance == 0:
-                yield words
+                output.append(' '.join(words))
             
             for edge in self.graph.edges_by_from_node[node]:
                 new_words = [*words, edge.word] if node.is_starting else [edge.word, *words]
-                yield from self._get_palindromes_recursively(edge.to_node, new_words, max_word_count - 1)
+                self._get_palindromes_recursively(edge.to_node, new_words, max_word_count - 1, output)
